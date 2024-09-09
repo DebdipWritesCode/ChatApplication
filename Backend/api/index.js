38c24ffc2944d09e125e0f2b7b4ad74e5ae6c7ad
DebdipWritesCode <debdipmukherjee52@gmail.com>
2024-09-10 02:20:37 +0530

    fix CORS issue

--- Backend/api/index.js
+++ Backend/api/index.js
@@ -1,19 +1,25 @@
 const express = require('express');
 const http = require('http');
+const cors = require('cors');
 const connectDB = require('../config'); 
 const { setupSocket } = require('../socket');
-const cors = require('cors');
 
 const app = express();
 const server = http.createServer(app);
 
-app.use(cors());
+const allowedOrigins = ['https://chat-application-jh8r.vercel.app'];
+
+app.use(cors({
+  origin: allowedOrigins,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['X-Requested-With', 'Content-Type', 'Accept', 'Origin', 'Authorization'],
+  credentials: true
+}));
 
 const apiRoutes = require('../routes/apiRoutes');
 app.use('/api', apiRoutes);
 
 connectDB();
-
 setupSocket(server);
 
 server.listen(3000, () => {
--- Backend/socket.js
+++ Backend/socket.js
@@ -4,19 +4,22 @@ const { handleSocketConnection } = require("./controllers/chatController");
 const setupSocket = (server) => {
   const io = new Server(server, {
     cors: {
-      origin: '*',
+      origin: 'https://chat-application-jh8r.vercel.app',
+      methods: ['GET', 'POST'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
+      credentials: true, 
     }
   });
 
   io.on("connection", (socket) => {
     console.log("A user connected");
-  
+
     handleSocketConnection(io, socket);
-  
+
     socket.on("disconnect", () => {
       console.log("A user disconnected");
     });
   });
-}
+};
 
 module.exports = { setupSocket };
